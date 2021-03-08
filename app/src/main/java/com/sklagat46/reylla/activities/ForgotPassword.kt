20949b6bd4840f5a872e630a16476f46830aef3b
sklagat45 <sklagat46@gmail.com>
2021-03-08 19:27:26 +0300

    feature_register_companyProvider

--- app/src/main/java/com/sklagat46/reylla/activities/ForgotPassword.kt
+++ app/src/main/java/com/sklagat46/reylla/activities/ForgotPassword.kt
@@ -1,12 +1,37 @@
 package com.sklagat46.reylla.activities
 
 import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
-import com.sklagat46.reylla.R
 
 class ForgotPassword : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_forgot_password)
-    }
+
+
+//    private var mAuth: FirebaseAuth? = null
+//
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//        setContentView(R.layout.activity_forgot_password)
+//
+//        mAuth = FirebaseAuth.getInstance()
+//
+//        btnResetPassword.setOnClickListener {
+//            val email = edtResetEmail.text.toString().trim()
+//
+//            if (TextUtils.isEmpty(email)) {
+//                Toast.makeText(applicationContext, "Enter your email!", Toast.LENGTH_SHORT).show()
+//            } else {
+//                mAuth!!.sendPasswordResetEmail(email)
+//                    .addOnCompleteListener { task ->
+//                        if (task.isSuccessful) {
+//                            Toast.makeText(this@ForgotPassword, "Check email to reset your password!", Toast.LENGTH_SHORT).show()
+//                        } else {
+//                            Toast.makeText(this@ForgotPassword, "Fail to send reset password email!", Toast.LENGTH_SHORT).show()
+//                        }
+//                    }
+//            }
+//        }
+//
+//        btnBack.setOnClickListener {
+//            finish()
+//        }
+//    }
 }
