3c57436a0ba2b4135f2252a9ed7ef3a58b210dcb
sklagat45 <sklagat46@gmail.com>
2021-04-27 03:54:52 +0300

    feature-fetch-services

---
+++ app/src/main/java/com/sklagat46/reylla/adapter/MakeupServiceAdapter.kt
@@ -0,0 +1,71 @@
+package com.sklagat46.reylla.adapter
+
+import android.content.Context
+import android.content.Intent
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.sklagat46.reylla.R
+import com.sklagat46.reylla.activities.serviceproviders.MakeUPActivity
+import com.sklagat46.reylla.activities.serviceproviders.ui.details.ServiceDetailsActivity
+import com.sklagat46.reylla.model.MakeupService
+import com.sklagat46.reylla.utils.Constants
+import com.sklagat46.reylla.utils.GlideLoader
+import kotlinx.android.synthetic.main.row_list_service_item.view.*
+
+class MakeupServiceAdapter(
+    private val context: Context,
+    private var list: ArrayList<MakeupService>,
+    private val activity: MakeUPActivity
+) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        return MakeupServiceAdapter.MakeupViewHolder(
+            LayoutInflater.from(context).inflate(
+                R.layout.row_list_service_item,
+                parent,
+                false
+            )
+        )
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        val model = list[position]
+
+        if (holder is MakeupServiceAdapter.MakeupViewHolder) {
+
+            GlideLoader(context).loadServicePicture(
+                model.mServiceImageURL,
+                holder.itemView.iv_item_service_image
+            )
+
+            holder.itemView.tv_item_service_name.text = model.styleName
+            holder.itemView.tv_item_duration.text = model.styleDuration
+
+            holder.itemView.tv_item_cost.text = "Ksh" + "$${model.styleCost}"
+
+//            holder.itemView.iv_item_delete.setOnClickListener {
+//
+//                activity.deleteService(model.service_id)
+//            }
+
+            holder.itemView.setOnClickListener {
+                // Launch Product details screen.
+                val intent = Intent(context, ServiceDetailsActivity::class.java)
+                intent.putExtra(Constants.EXTRA_SERVICE_ID, model.service_id)
+                intent.putExtra(Constants.EXTRA_SERVICE_OWNER_ID, model.providerID)
+                context.startActivity(intent)
+            }
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return list.size
+    }
+
+    class MakeupViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+
+    }
+
+}
---
+++ app/src/main/java/com/sklagat46/reylla/adapter/TatColorServiceAdapter.kt
@@ -0,0 +1,72 @@
+package com.sklagat46.reylla.adapter
+
+import android.content.Context
+import android.content.Intent
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.sklagat46.reylla.R
+import com.sklagat46.reylla.activities.serviceproviders.TatooAndColorActivity
+import com.sklagat46.reylla.activities.serviceproviders.ui.details.ServiceDetailsActivity
+import com.sklagat46.reylla.model.TatColorService
+import com.sklagat46.reylla.utils.Constants
+import com.sklagat46.reylla.utils.GlideLoader
+import kotlinx.android.synthetic.main.row_list_service_item.view.*
+
+class TatColorServiceAdapter(
+    private val context: Context,
+    private var list: ArrayList<TatColorService>,
+    private val activity: TatooAndColorActivity
+) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        return TatColorServiceAdapter.TatooAndColorViewHolder(
+            LayoutInflater.from(context).inflate(
+                R.layout.row_service_item,
+                parent,
+                false
+            )
+        )
+    }
+
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        val model = list[position]
+
+        if (holder is TatColorServiceAdapter.TatooAndColorViewHolder) {
+
+            GlideLoader(context).loadServicePicture(
+                model.mServiceImageURL,
+                holder.itemView.iv_item_service_image
+            )
+
+            holder.itemView.tv_item_service_name.text = model.styleName
+            holder.itemView.tv_item_duration.text = model.styleDuration
+
+            holder.itemView.tv_item_cost.text = "Ksh" + "$${model.styleCost}"
+
+//            holder.itemView.iv_item_delete.setOnClickListener {
+//
+//                activity.deleteService(model.service_id)
+//            }
+
+            holder.itemView.setOnClickListener {
+                // Launch Product details screen.
+                val intent = Intent(context, ServiceDetailsActivity::class.java)
+                intent.putExtra(Constants.EXTRA_SERVICE_ID, model.service_id)
+                intent.putExtra(Constants.EXTRA_SERVICE_OWNER_ID, model.providerID)
+                context.startActivity(intent)
+            }
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return list.size
+    }
+
+    class TatooAndColorViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+
+    }
+
+}
