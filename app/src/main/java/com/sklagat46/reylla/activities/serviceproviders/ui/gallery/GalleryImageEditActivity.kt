c27d811936175351dbf24931aac8fcb75cb16190
samuel kiplagat <sklagat46@gmail.com>
2021-06-15 01:18:47 +0300

    feature-fetch-services

---
+++ app/src/main/java/com/sklagat46/reylla/activities/serviceproviders/ui/gallery/GalleryImageEditActivity.kt
@@ -0,0 +1,12 @@
+package com.sklagat46.reylla.activities.serviceproviders.ui.gallery
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.sklagat46.reylla.R
+
+class GalleryImageEditActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_gallery_image_edit)
+    }
+}
